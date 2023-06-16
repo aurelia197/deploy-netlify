063e6f080ce309802d26950290d9c6a8117d334e
Aurelia <85263622+aurelia197@users.noreply.github.com>
2023-06-16 15:02:39 +0700

    Deploy Netlify

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Aurelia Nur Salsabilla" // TODO: replace this
+const studentId = "FE5127209" // TODO: replace this
+
+export { url, studentName, studentId }
